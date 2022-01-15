cdd17db1a580a55cf6cefe01bc8d3be8671f6c12
moritzfelipe <stellmacher@gmail.com>
2022-01-15 13:50:29 +0100

    Seperate connect wallet component from navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,55 +1,11 @@
-import { useEffect, useState } from "react";
-import Web3Modal from "web3modal";
-import { ethers } from "ethers";
-
-const providerOptions = {
-  /* See Provider Options Section */
-};
-
-let web3Modal = null;
+import WalletConnect from "./WalletConnect";
 
 export default function Navbar() {
-  const [address, setAddress] = useState('');
-
-  const initWeb3 = () => {
-    if (typeof window !== "undefined") {
-      web3Modal = new Web3Modal({
-        network: "mainnet", // optional
-        cacheProvider: true, // optional
-        providerOptions, // required
-      });
-    }
-  };
-
-  const connect = async () => {
-    const provider = new ethers.providers.Web3Provider(
-      await web3Modal.connect()
-    );
-    const signer = provider.getSigner();
-    setAddress(await signer.getAddress());
-  };
-
-  useEffect(() => {
-    initWeb3();
-    if (web3Modal && web3Modal.cachedProvider) {
-      connect();
-    }
-  }, []);
-
   return (
     <div className="flex justify-between w-10/12 mx-auto py-6">
       <div className="text-2xl">NFTCerts</div>
       <div>
-        {address ? (
-          <span className="bg-gray-200 px-6 py-2 rounded-full">{address}</span>
-        ) : (
-          <button
-            onClick={connect}
-            className="bg-gray-200 px-6 py-2 rounded-full"
-          >
-            Connect
-          </button>
-        )}
+        <WalletConnect />
       </div>
     </div>
   );
